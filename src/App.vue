7cf3b7bf4dd927ff4f0b97c797d0f5c0a3934d9f
Theam-Dev <phearakroun@gmail.com>
2023-05-15 21:21:21 +0700

    Slide Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 style="margin-bottom: 40px;">Vue 2 Slider using vue-slider-component</h1>
+    <vue-slider v-model="value" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    VueSlider,
+  },
+
+  data() {
+    return {
+      value: 0,
+    }
+  },
 }
 </script>
 
 <style>
 #app {
+  padding: 20px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.vue-slider{
+  height: 25px !important;
+}
+.vue-slider-dot{
+  width: 25px !important;
+  height: 25px !important;
 }
 </style>
